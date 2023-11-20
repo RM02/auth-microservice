f306084815ac1c7cbe76c9770d2bb37328c240e0
RM02 <rafamark244@gmail.com>
2023-11-20 14:48:08 -0400

    settings

--- authservice/wsgi_handler.py
+++ authservice/wsgi_handler.py
@@ -2,7 +2,7 @@
 
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'authservice.settings'
 
 from django.core.wsgi import get_wsgi_application
 
