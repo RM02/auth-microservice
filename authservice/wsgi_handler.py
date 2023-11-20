f8b6ec36fc0edaab3d894405b3183a329a372e17
RM02 <rafamark244@gmail.com>
2023-11-20 14:39:45 -0400

    fixed wsgi handler

--- authservice/wsgi_handler.py
+++ authservice/wsgi_handler.py
@@ -2,8 +2,8 @@
 
 import os
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'authservice.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_appli
+application = get_wsgi_application()
